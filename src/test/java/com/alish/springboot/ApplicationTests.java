cbd3ee57e4c8a4e1f3927ac6d01b6d77b397c0eb
alish <alish.mirhashemi@gmail.com>
2023-05-15 04:17:51 +0330

    first cimmit

---
+++ src/test/java/com/alish/springboot/ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.alish.springboot;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ApplicationTests {
+
+    @Test
+    @Disabled
+    void contextLoads() {
+    }
+
+}
